6cfecde690cefa69925aa9af50460b070743e99a
AyeshaSiddiqua68 <akhiazad6868@gmail.com>
2021-11-14 23:13:45 +0600

    add and delete part done

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const ObjectId = require('mongodb').ObjectId;
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { application } = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,8 +13,6 @@ app.get('/', (req, res) => {
     res.send('DJI drone is running')
 })
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ksgka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -24,6 +21,8 @@ async function run() {
         await client.connect();
         const database = client.db("DJI-Drone-BD");
         const drones_Collection = database.collection("drones");
+        const cart_Collection = database.collection("cart");
+
 
         //load drones (GET API)
         app.get("/drones", async (req, res) => {
@@ -50,6 +49,44 @@ async function run() {
             const drone = await drones_Collection.findOne(query);
             res.json(drone);
         });
+
+        //load cart information by uid (GET API)
+        app.get("/cart/:uid", async (req, res) => {
+            const uid = req.params.uid;
+            const query = { uid: uid };
+            const result = await cart_Collection.find(query).toArray();
+            res.json(result);
+        });
+
+
+        //add data and extra info to cart
+        app.post("/drone/add", async (req, res) => {
+            const drone = req.body;
+            const result = await cart_Collection.insertOne(drone);
+            res.json(result)
+        });
+
+        //delete data from cart (DELETE API)
+        app.delete("/delete/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await cart_Collection.deleteOne(query);
+            res.json(result);
+        });
+
+        //for purchase button (DELETE API)
+        app.delete("/purchase/:uid", async (req, res) => {
+            const uid = req.params.uid;
+            const query = { uid: uid };
+            const result = await cart_Collection.deleteMany(query);
+            res.json(result);
+
+        })
+
+
+
+
+
 
     } finally {
         //   await client.close();
